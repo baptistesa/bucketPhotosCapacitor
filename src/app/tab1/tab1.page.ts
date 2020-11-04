9c392c13f25ed85e0eeacce7c2c73a121732a5f7
Baptiste Saint-André <baptiste.saintandre@gmail.com>
2020-11-04 10:49:18 +0100

    Bucket project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
+import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class Tab1Page {
+  private pictureOpts: CameraPreviewPictureOptions = {
+    width: 1280,
+    height: 1280,
+    quality: 85
+  }
+  private picture;
 
-  constructor() {}
+  constructor(private cameraPreview: CameraPreview, private platform: Platform, private http: HttpClient) {
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: true,
+      alpha: 1
+    }
+
+    alert('ok');
+
+    // start camera
+    this.cameraPreview.startCamera(cameraPreviewOpts).then(
+      (res) => {
+        alert("success " + res)
+      },
+      (err) => {
+        alert("error " + err)
+      });
+  }
+
+  // Take a picture
+  takePic() {
+    if (this.platform.is("ios")) {
+      this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+        this.picture = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        console.log(err);
+        this.picture = 'assets/img/test.jpg';
+      });
+    }
+    else if (this.platform.is("android")) {
+      this.cameraPreview.takeSnapshot(this.pictureOpts).then((imageData) => {
+        this.picture = 'data:image/jpeg;base64,' + imageData;
+        this.sendPicture(this.picture)
+      }, (err) => {
+        console.log(err);
+        this.picture = 'assets/img/test.jpg';
+      });
+    }
+
+  }
+
+  // Send picture to backend
+  sendPicture(image) {
+    let body = {
+      owner: "Baptiste",
+      image: image
+    };
+    this.http.post("https://4a519ccb.ngrok.io/sendPic", body)
+      .subscribe(data => {
+        alert('image uploadé !');
+      })
+  }
 
 }
