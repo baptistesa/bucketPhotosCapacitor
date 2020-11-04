96959ab10c9827a834f1ac12d8f607b8a2250361
Baptiste Saint-André <baptiste.saintandre@gmail.com>
2020-11-04 11:15:26 +0100

    Update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  pictures = [];
+
+  constructor(private http : HttpClient, private platform : Platform) {
+    
+  }
+
+  ngAfterViewInit() {
+    this.retrievePics();
+  }
+
+  retrievePics() {
+    this.http.get("https://4a519ccb.ngrok.io/getPics")
+      .subscribe(data => {
+        this.pictures = JSON.parse(JSON.stringify(data));
+        // for(let pic of this.pictures) {
+        //   pic.image = "data:image/jpeg;base64, " + pic.image;
+        // }
+      })
+  }
 
 }
